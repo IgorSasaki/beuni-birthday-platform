d730a342253d0f11e7048fbf51bd8d6084d9ae6b
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 00:55:31 -0300

    feat(app funcionarios novo _components form): implement form section

---
+++ src/app/(logged)/funcionarios/novo/_components/Form/types.ts
@@ -0,0 +1,14 @@
+export interface EmployeeFormData {
+  birthDate: string
+  cep: string
+  city: string
+  complement?: string
+  department: string
+  fullName: string
+  giftSize: 'P' | 'M' | 'G' | 'GG' | 'XG'
+  neighborhood: string
+  number: string
+  position: string
+  state: string
+  street: string
+}
