1bbd78ca7fb700ac988e7949582c12a0545712e4
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 00:52:19 -0300

    feat(constants jobtitle): implement constants of jobtitle

---
+++ src/constants/jobTitle.ts
@@ -0,0 +1,176 @@
+export const JOB_TITLE_BY_GROUP = [
+  {
+    group: 'Administrativo',
+    items: [
+      { label: 'Assistente Administrativo', value: 'administrative-assistant' },
+      { label: 'Secretária Executiva', value: 'executive-secretary' },
+      { label: 'Recepcionista', value: 'receptionist' },
+      { label: 'Auxiliar de Escritório', value: 'office-clerk' },
+      {
+        label: 'Coordenador Administrativo',
+        value: 'administrative-coordinator'
+      },
+      { label: 'Gerente Administrativo', value: 'administrative-manager' },
+      { label: 'Analista Administrativo', value: 'administrative-analyst' },
+      { label: 'Controlador de Documentos', value: 'document-controller' },
+      {
+        label: 'Auxiliar de Serviços Gerais',
+        value: 'general-services-assistant'
+      },
+      { label: 'Office Boy', value: 'office-runner' }
+    ]
+  },
+  {
+    group: 'Tecnologia da Informação',
+    items: [
+      { label: 'Desenvolvedor Front-end', value: 'frontend-developer' },
+      { label: 'Desenvolvedor Back-end', value: 'backend-developer' },
+      { label: 'Tech Lead', value: 'tech-lead' },
+      { label: 'Arquiteto de Software', value: 'software-architect' },
+      { label: 'Engenheiro DevOps', value: 'devops-engineer' },
+      { label: 'Analista de Suporte', value: 'support-analyst' },
+      { label: 'Product Manager', value: 'product-manager' },
+      { label: 'Scrum Master', value: 'scrum-master' },
+      { label: 'Analista de QA', value: 'qa-analyst' },
+      {
+        label: 'Especialista em Segurança da Informação',
+        value: 'infosec-specialist'
+      }
+    ]
+  },
+  {
+    group: 'Recursos Humanos',
+    items: [
+      { label: 'Analista de RH', value: 'hr-analyst' },
+      { label: 'Assistente de RH', value: 'hr-assistant' },
+      { label: 'Coordenador de RH', value: 'hr-coordinator' },
+      { label: 'Business Partner', value: 'hr-business-partner' },
+      { label: 'Gerente de RH', value: 'hr-manager' },
+      { label: 'Recrutador', value: 'recruiter' },
+      {
+        label: 'Psicólogo Organizacional',
+        value: 'organizational-psychologist'
+      },
+      { label: 'Consultor de RH', value: 'hr-consultant' },
+      {
+        label: 'Especialista em Folha de Pagamento',
+        value: 'payroll-specialist'
+      },
+      { label: 'Treinamento e Desenvolvimento', value: 'training-development' }
+    ]
+  },
+  {
+    group: 'Financeiro',
+    items: [
+      { label: 'Assistente Financeiro', value: 'financial-assistant' },
+      { label: 'Analista Financeiro', value: 'financial-analyst' },
+      { label: 'Contador', value: 'accountant' },
+      { label: 'Controller', value: 'controller' },
+      { label: 'Tesoureiro', value: 'treasurer' },
+      { label: 'Coordenador Financeiro', value: 'financial-coordinator' },
+      { label: 'Gerente Financeiro', value: 'financial-manager' },
+      { label: 'Especialista em Faturamento', value: 'billing-specialist' },
+      { label: 'Analista de Custos', value: 'cost-analyst' },
+      {
+        label: 'Analista de Crédito e Cobrança',
+        value: 'credit-collections-analyst'
+      }
+    ]
+  },
+  {
+    group: 'Marketing',
+    items: [
+      { label: 'Analista de Marketing', value: 'marketing-analyst' },
+      { label: 'Coordenador de Marketing', value: 'marketing-coordinator' },
+      {
+        label: 'Especialista em Redes Sociais',
+        value: 'social-media-specialist'
+      },
+      { label: 'Redator', value: 'copywriter' },
+      { label: 'Designer Gráfico', value: 'graphic-designer' },
+      { label: 'Videomaker', value: 'videomaker' },
+      { label: 'Gerente de Marketing', value: 'marketing-manager' },
+      { label: 'Especialista em SEO', value: 'seo-specialist' },
+      { label: 'Especialista em Mídia Paga', value: 'paid-media-specialist' },
+      { label: 'Produtor de Conteúdo', value: 'content-producer' }
+    ]
+  },
+  {
+    group: 'Vendas',
+    items: [
+      { label: 'Vendedor Interno', value: 'inside-salesperson' },
+      { label: 'Vendedor Externo', value: 'outside-salesperson' },
+      { label: 'Representante Comercial', value: 'sales-representative' },
+      { label: 'Coordenador de Vendas', value: 'sales-coordinator' },
+      { label: 'Gerente de Vendas', value: 'sales-manager' },
+      { label: 'Analista Comercial', value: 'commercial-analyst' },
+      { label: 'Consultor de Vendas', value: 'sales-consultant' },
+      { label: 'Promotor de Vendas', value: 'sales-promoter' },
+      { label: 'Pré-vendas (SDR)', value: 'sales-development-rep' },
+      { label: 'Pós-vendas (CS)', value: 'customer-success' }
+    ]
+  },
+  {
+    group: 'Suporte Técnico',
+    items: [
+      { label: 'Suporte Nível 1', value: 'support-analyst-level-1' },
+      { label: 'Suporte Nível 2', value: 'support-analyst-level-2' },
+      { label: 'Técnico de Informática', value: 'it-technician' },
+      { label: 'Help Desk', value: 'help-desk' },
+      { label: 'Instalador de Sistemas', value: 'system-installer' },
+      {
+        label: 'Especialista em Suporte Técnico',
+        value: 'technical-support-specialist'
+      },
+      { label: 'Coordenador de Suporte', value: 'support-coordinator' },
+      { label: 'Gerente de Suporte', value: 'support-manager' },
+      { label: 'Analista de Sistemas', value: 'system-support-analyst' },
+      { label: 'Monitor de Qualidade', value: 'quality-monitor' }
+    ]
+  },
+  {
+    group: 'Logística',
+    items: [
+      { label: 'Auxiliar de Logística', value: 'logistics-assistant' },
+      { label: 'Conferente', value: 'goods-checker' },
+      { label: 'Motorista', value: 'driver' },
+      { label: 'Entregador', value: 'delivery-person' },
+      { label: 'Analista de Logística', value: 'logistics-analyst' },
+      { label: 'Coordenador de Logística', value: 'logistics-coordinator' },
+      { label: 'Gerente de Logística', value: 'logistics-manager' },
+      { label: 'Estoquista', value: 'stock-keeper' },
+      { label: 'Supervisor de Frota', value: 'fleet-supervisor' },
+      { label: 'Operador de Empilhadeira', value: 'forklift-operator' }
+    ]
+  },
+  {
+    group: 'Jurídico',
+    items: [
+      { label: 'Advogado', value: 'lawyer' },
+      { label: 'Estagiário Jurídico', value: 'legal-intern' },
+      { label: 'Coordenador Jurídico', value: 'legal-coordinator' },
+      { label: 'Gerente Jurídico', value: 'legal-manager' },
+      { label: 'Assistente Jurídico', value: 'legal-assistant' },
+      { label: 'Analista Contratual', value: 'contract-analyst' },
+      { label: 'Analista de Compliance', value: 'compliance-analyst' },
+      { label: 'Consultor Jurídico', value: 'legal-consultant' },
+      { label: 'Procurador', value: 'prosecutor' },
+      { label: 'Paralegal', value: 'paralegal' }
+    ]
+  },
+  {
+    group: 'Outros',
+    items: [
+      { label: 'Estagiário', value: 'intern' },
+      { label: 'Jovem Aprendiz', value: 'young-apprentice' },
+      { label: 'Freelancer', value: 'freelancer' },
+      { label: 'Voluntário', value: 'volunteer' },
+      { label: 'Consultor Externo', value: 'external-consultant' },
+      { label: 'Sócio de Negócios', value: 'business-partner' },
+      { label: 'Pesquisador', value: 'researcher' },
+      { label: 'Instrutor', value: 'instructor' },
+      { label: 'Empreendedor', value: 'entrepreneur' },
+      { label: 'Outro', value: 'other' }
+    ]
+  }
+] as const
