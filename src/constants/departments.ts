145399b0a9d96103f80c22c8e0d30054feacab56
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 00:52:56 -0300

    feat(constants departments): implement constants of departments

---
+++ src/constants/departments.ts
@@ -0,0 +1,11 @@
+export const DEPARTMENTS = [
+  { label: 'Recursos Humanos', value: 'human-resources' },
+  { label: 'Tecnologia da Informação', value: 'information-technology' },
+  { label: 'Financeiro', value: 'finance' },
+  { label: 'Marketing', value: 'marketing' },
+  { label: 'Vendas', value: 'sales' },
+  { label: 'Suporte Técnico', value: 'technical-support' },
+  { label: 'Logística', value: 'logistics' },
+  { label: 'Jurídico', value: 'legal' },
+  { label: 'Outros', value: 'others' }
+] as const
