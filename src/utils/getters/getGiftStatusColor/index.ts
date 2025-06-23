2825d88c493d8aa2345bf124be778b704c0cb3a8
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-23 15:16:10 -0300

    fix(utils getters getgiftstatuscolor): fix text status

--- src/utils/getters/getGiftStatusColor/index.ts
+++ src/utils/getters/getGiftStatusColor/index.ts
@@ -3,7 +3,7 @@ export const getGiftStatusColor = (status: string) => {
     {
       NOT_REQUESTED: 'bg-gray-100 text-gray-800',
       PENDING: 'bg-yellow-100 text-yellow-800',
-      SENDING: 'bg-blue-100 text-blue-800',
+      SENT: 'bg-blue-100 text-blue-800',
       DELIVERED: 'bg-green-100 text-green-800'
     }[status] || 'bg-gray-100 text-gray-800'
   )
