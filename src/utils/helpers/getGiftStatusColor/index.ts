50953fe7d60983f9b7054e417f60a5fcc50e2a41
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 23:39:44 -0300

    feat(utils helpers): add status of gift

--- src/utils/helpers/getGiftStatusColor/index.ts
+++ src/utils/helpers/getGiftStatusColor/index.ts
@@ -1,6 +1,7 @@
 export const getGiftStatusColor = (status: string) => {
   return (
     {
+      NOT_REQUESTED: 'bg-gray-100 text-gray-800',
       PENDING: 'bg-yellow-100 text-yellow-800',
       SENDING: 'bg-blue-100 text-blue-800',
       DELIVERED: 'bg-green-100 text-green-800'
--- src/utils/helpers/getGiftStatusText/index.ts
+++ src/utils/helpers/getGiftStatusText/index.ts
@@ -1,6 +1,7 @@
 export const getGiftStatusText = (status: string) => {
   return (
     {
+      NOT_REQUESTED: 'Não solicitado',
       PENDING: 'Pendente',
       SENDING: 'Enviado',
       DELIVERED: 'Entregue'
