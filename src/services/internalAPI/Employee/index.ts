42c55847f39157a0d7136ac633bdb733261e00eb
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 23:40:35 -0300

    feat(services internalapi employee): implement service class to handler requests

---
+++ src/services/internalAPI/Employee/index.ts
@@ -0,0 +1,44 @@
+import { AxiosInstance, AxiosResponse } from 'axios'
+
+import { BirthdayFilter } from '@/app/(logged)/funcionarios/types'
+
+export class Employee {
+  private instance: AxiosInstance
+
+  constructor(instance: AxiosInstance) {
+    this.instance = instance
+  }
+
+  public async getAllEmployees(
+    token: string,
+    searchTerm: string,
+    filters: BirthdayFilter = {}
+  ): Promise<AxiosResponse> {
+    const { department, status, month } = filters
+
+    const cleanedFilters: BirthdayFilter = {
+      month:
+        typeof month === 'string' && month !== 'ALL'
+          ? parseInt(month, 10)
+          : undefined,
+      department: department && department !== 'ALL' ? department : undefined,
+      status: status && status !== 'ALL' ? status : undefined
+    }
+
+    const params = {
+      searchText: searchTerm || undefined,
+      ...cleanedFilters
+    }
+
+    return this.instance.get('/employees', {
+      params,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+  }
+
+  public async getEmployeeById(employeeId: string): Promise<AxiosResponse> {
+    return this.instance.get(`/employees/${employeeId}`)
+  }
+}
