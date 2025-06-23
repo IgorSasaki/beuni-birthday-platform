297a8d32f7915694ddbc35e9973d9074fc590e4a
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 23:41:06 -0300

    feat(services internalapi): add service class to internal api instances

--- src/services/internalAPI/index.ts
+++ src/services/internalAPI/index.ts
@@ -2,12 +2,14 @@ import axios, { AxiosInstance } from 'axios'
 
 import { Auth } from './Auth'
 import { Dashboard } from './Dashboard'
+import { Employee } from './Employee'
 
 export class InternalAPI {
   private instance: AxiosInstance
 
   auth: Auth
   dashboard: Dashboard
+  employee: Employee
 
   constructor() {
     this.instance = axios.create({
@@ -16,5 +18,6 @@ export class InternalAPI {
 
     this.auth = new Auth(this.instance)
     this.dashboard = new Dashboard(this.instance)
+    this.employee = new Employee(this.instance)
   }
 }
