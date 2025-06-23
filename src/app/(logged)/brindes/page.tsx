1117ffbeb47dd25206513a72bf40f60264a41970
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-23 01:33:51 -0300

    feat(app brindes): implement integration withg API

--- src/app/(logged)/brindes/page.tsx
+++ src/app/(logged)/brindes/page.tsx
@@ -1,20 +1,51 @@
-import { NextPage } from 'next'
+'use client'
 
+import { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import { internalAPIInstance } from '@/instances/internalAPI'
+import { Gift } from '@/models/Gifts'
+
+import { BirthdayFilter } from '../funcionarios/types'
 import { Filters } from './_components/Filters'
 import { GiftsList } from './_components/GiftsList'
 import { Header } from './_components/Header'
 import { StatsCards } from './_components/StatsCards'
 
-const Page: NextPage = async () => {
+const Page: NextPage = () => {
+  const [token] = useLocalStorage('auth_token', '')
+
+  const [searchTerm, setSearchTerm] = useState('')
+  const [filters, setFilters] = useState<BirthdayFilter>({})
+
+  const [gifts, setGifts] = useState<Gift[]>([])
+
+  useEffect(() => {
+    internalAPIInstance.gifts
+      .listGifts(token, searchTerm, filters)
+      .then(response => {
+        setGifts(response.data)
+      })
+      .catch(error => {
+        console.error({ listGiftsError: error })
+      })
+  }, [token, searchTerm, filters])
+
   return (
     <div className="space-y-8">
       <Header />
 
       <StatsCards />
 
-      <Filters />
+      <Filters
+        filters={filters}
+        searchTerm={searchTerm}
+        setFilters={setFilters}
+        setSearchTerm={setSearchTerm}
+      />
 
-      <GiftsList />
+      <GiftsList filters={filters} gifts={gifts} searchTerm={searchTerm} />
     </div>
   )
 }
