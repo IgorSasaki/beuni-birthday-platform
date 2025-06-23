991e4724dd909627f2658f03a18755cb350001ce
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 23:41:41 -0300

    feat(constants giftstatus): implement constants of gifts status

---
+++ src/constants/gifttStatus.ts
@@ -0,0 +1,6 @@
+export const GIFT_STATUS = [
+  { value: 'NOT_REQUESTED', label: 'Não solicitado' },
+  { value: 'PENDING', label: 'Pendente' },
+  { value: 'SEND', label: 'Enviado' },
+  { value: 'DELIVERED', label: 'Entregue' }
+] as const
