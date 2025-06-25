5baced5917f8df6b046f535f6d2546b060265419
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-24 22:45:44 -0300

    fix(app functionarios [id] editar): adjust component

--- src/app/(logged)/funcionarios/[id]/editar/page.tsx
+++ src/app/(logged)/funcionarios/[id]/editar/page.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 import { EmployeePageProps } from '../types'
 import { Container } from './_components/Container'
 
-const Page: NextPage<EmployeePageProps> = async ({ params }) => {
+const Page: NextPage<EmployeePageProps> = ({ params }) => {
   const { id } = params
 
   return <Container id={id} />
